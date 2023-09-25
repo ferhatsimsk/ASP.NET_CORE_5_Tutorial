f61989e99e7e70b2fc784fa45ad9f8556dc4d0f6
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-09-25 22:44:14 +0300

    Added MVC Tutorial Notes

---
+++ 2-MVC_1/OrnekMVC_1/OrnekMVC_1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekMVC_1.Controllers
+{
+
+    //Bir sınıfı request alabilir ve response döndürebilir yani controller yapabilmek için o sınıfı controller class'ından türetmemiz gerekmektedir.
+    //Controller sınıfları içerisinde istekleri karşılayan metotlara action metot denir.
+    //Controller sınıfları içerisinde tanımlanan tüm metotlar artık action metot olarak nitelendirilecektir.
+    
+    
+    public class HomeController : Controller //Buradaki controller action metot yapar.Eğer bu olmazsa normal metot olur.
+    {
+        //Action metot : Controllera gelen isteği karşılayan ve gerekli operasyonları gerçekleştien metotlardır.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2-MVC_1/OrnekMVC_1/OrnekMVC_1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekMVC_1.Models;
+
+namespace OrnekMVC_1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            Product product = new Product(); //Conroller'dan model'a gitmen anlamına gelir. new leme işlemi. Model'a gidip oradan veriyi alıp getirmesi.
+
+            //Veri retildi...
+            //View fonksiyonu bu actiona ait view (.cshtml) dosyasını tetikleyecek olan fonksiyondur.
+            
+            //ViewResult result=View(); ==> ilgili action ismiyle aynı olan viewi tetikler.
+            ViewResult result = View("Ahmet"); //==> belirtilen view ismindeki view dosyasını rander eder. Model'a giderse eğer ordan aldığı veriyi geri döndürmesi. Ama model'a illaki gidecek diye bir kural yok.
+
+            //return View();
+            return result;
+        }
+
+    }
+}
