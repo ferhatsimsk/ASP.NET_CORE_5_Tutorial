c32bd2cb1f6fa69b517151575d1e286596467fb7
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-10-11 10:38:06 +0300

    Added Custom HtmlHelper and TagHelper

---
+++ 9-Custom HtmlHelper Fonksiyon Oluşturmak/9-Custom HtmlHelper Fonksiyon Oluşturmak/OrnekMVC_1/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrnekMVC_1.Models;
+using OrnekMVC_1.Models.ViewModels;
+using System.Collections.Generic;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+
+namespace OrnekMVC_1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult GetProducts()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                ProductName = "A Product",
+                Quantity = 15,
+
+            };
+
+            User user = new User()
+            {
+                Id = 1,
+                Name = "Gençay",
+                LastName = "Yıldız",
+
+            };
+
+            //UserProduct userProduct = new UserProduct
+            //{
+            //    User = user,
+            //    Product = product,
+            //};
+
+            var userProduct = (product, user);
+
+            //return View(userProduct);
+            return View(userProduct);
+        }
+    }
+}
---
+++ 9-Custom HtmlHelper Fonksiyon Oluşturmak/9-Custom HtmlHelper Fonksiyon Oluşturmak/OrnekMVC_1/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OrnekMVC_1.Extensions
+{
+    static public class Extensions
+    {
+        //Custom bir HtmlHelper oluşturma.
+        //Diğer metotlarında custom halini extension fonksiyonuyla oluşturabiliriz.
+        //Bir şeyin custom halini oluşturmak istiyorsak extension fonksiyonlarını kullanabiliriz.
+        public static IHtmlContent CustomTextBox(this IHtmlHelper htmlHelper, string name, string value = "", string placeHolder = "")
+        {
+            return htmlHelper.TextBox(name, value, new
+            {
+                style="background-color:green;color:white;font-size:11px;",
+                @class ="form-input",
+                a="a",
+                b="b",
+                placehodler=placeHolder
+            });
+        }
+    }
+}
