648ef37bbc2ff9eb59782ac03a7da45b56cf918e
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-10-17 15:35:13 +0300

    Added Custom TagHelpers and Model Binding
    Added Custom TagHelpers and Model Binding

---
+++ 12-Model Binding/OrnekMVC_1/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrnekMVC_1.Models;
+using OrnekMVC_1.Models.ViewModels;
+using System.Collections.Generic;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+
+namespace OrnekMVC_1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct() 
+        {
+            var product=new Product()
+            { 
+                ProductName="B Product",
+                Quantity=25
+            };  
+            return View(product);
+        }
+
+        [HttpPost]
+        //public IActionResult CreateProduct(string txtProductName, string txtQuantity)
+        public IActionResult CreateProduct(Product product)
+        {
+            return View();
+        }
+    }
+}
