7cce8c80b42c2f76bc4a89879ed764f628234e16
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-10-23 20:37:30 +0300

    Header and Ajax
    Added Ajax Based Data Retrieval Method and Receiving Data via Header

---
+++ 15-Header Üzerinden Veri Alma/OrnekMVC_1/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrnekMVC_1.Models;
+using OrnekMVC_1.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+
+namespace OrnekMVC_1.Controllers
+{
+
+    //2.Yöntemin 
+    public class RouteData
+    {
+        public int A { get; set; }
+        public string B { get; set; }
+        public string Id { get; set; }
+
+    }
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+
+            return View();
+        }
+
+        
+        //Postman üzerinden işlem yaptık.
+        
+        public IActionResult VeriAl()
+        {
+            var headers=Request.Headers.ToList();
+            return View();
+        }
+    }
+}
