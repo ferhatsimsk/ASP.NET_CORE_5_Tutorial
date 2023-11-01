053e776535a354c2c0cc299668fe12b1e7897c2c
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-11-01 19:05:11 +0300

    Added Validation with ModelMetaType and Fluent Validation
    Added Validation with ModelMetaType and Fluent Validation

---
+++ 19-ModelMetaType ile Validation/OrnekMVC_1/Models/ModelMetadataTypes/ProductMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrnekMVC_1.Models.ModelMetadataTypes
+{
+    public class ProductMetadata
+    {
+        [Required (ErrorMessage ="Lütfen product name'i giriniz")] 
+        [StringLength(100, ErrorMessage = "Lütfen Product name'i en fazla 100 karakter olarak giriniz.")]
+        public string ProductName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Lütfen doğru bir email adresi giriniz.")]
+        public string Email { get; set; }
+
+
+    }
+}
---
+++ 19-ModelMetaType ile Validation/OrnekMVC_1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekMVC_1.Models.ModelMetadataTypes;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrnekMVC_1.Models
+{
+    [ModelMetadataType(typeof(ProductMetadata))]
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Email { get; set; }
+    }
+}
