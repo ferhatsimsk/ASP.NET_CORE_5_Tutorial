a3204050e9bd9d038a8cbc4c0feb55c6c714db28
ferhatsimsk <143225386+ferhatsimsk@users.noreply.github.com>
2023-11-03 19:06:19 +0300

    Added Layout and Viewstart & ViewImports

---
+++ 21-Server'daki Validaton'ları Dinamik olarak Client'a taşıma/OrnekMVC_1/Models/Validators/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace OrnekMVC_1.Models.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Email).NotNull().WithMessage("Email boş olamaz");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Ltfen doğru bir email giriniz");
+
+            RuleFor(x => x.ProductName).NotNull().WithMessage("Lütfen product name'i boş bırakılamaz").NotEmpty().WithMessage("Lütfen product name'i boş geçmeyiniz");
+            RuleFor(x => x.ProductName).MaximumLength(100).WithMessage("Lütfen product name'i 100 karakterden fazla girmeyiniz");
+
+
+        }
+
+    }
+}
